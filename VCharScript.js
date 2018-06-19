d11e7ae4817975f9f45cd96ab11115e82ac93b79
Samuel A. Lamb <samuei@users.noreply.github.com>
2018-06-18 20:10:48 -0500

    Dot Behavior
    Clicking a dot should now darken it and any lower-level dots for the same skill/attribute, as well as erasing any higher-level dots.

--- VCharScript.js
+++ VCharScript.js
@@ -8,6 +8,8 @@ window.addEventListener("load", function(event) {
 	}
 });
 
+var numberOfCheckboxes = 5; // Magic number: change 5 -> x to have x checkboxes. Be wary of spacing.
+
 function generateCheckboxes (element) {
 	// Create label for checkboxes based off of id
 	var label = document.createElement('label')
@@ -17,12 +19,26 @@ function generateCheckboxes (element) {
 	element.appendChild(label);
 	
 	// Create actual checkboxes
-	for (var i = 1; i <= 5; i++) { // Magic number: change 5 -> x to have x checkboxes. Be wary of spacing.
+	for (var i = 1; i <= numberOfCheckboxes; i++) { 
 		var checkbox = document.createElement('input');
 		checkbox.type = "checkbox";
 		checkbox.name = element.id + '_' + i;
 		checkbox.value = 'value';
 		checkbox.className = 'dot-checkbox';
+		checkbox.onclick = checkRelevantBoxes;
 		element.appendChild(checkbox);
+	}
+}
+
+function checkRelevantBoxes (event) { 
+	var prefix = event.currentTarget.name.substr(0, event.currentTarget.name.length - 1), // e.g. computer_
+		position = event.currentTarget.name.substr(-1);
+	
+	for (var i = 1; i <= numberOfCheckboxes; i++) { 
+		var underDotEl = document.getElementsByName(prefix + i)[0];
+		
+		if (underDotEl) { 
+			underDotEl.checked = i <= position;
+		}
 	}
 }
